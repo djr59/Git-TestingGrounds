39c057e3d1adc21e2dbadd9457ab9d205b013606
dan Muro <enginenear123@gmail.com>
2018-08-08 22:53:22 -0400

    edit text

--- forLoop.cpp
+++ forLoop.cpp
@@ -3,15 +3,19 @@ using namespace std;
 
 int main()
 {
+    bool linkIsSad = true;
 
     int x = 10;
 
+    if (linkIsSad){
+        x = 20;
+    }
     for ( int i = 0; i < 10; i++){
         x++;
 
 
     }
 
-    cout << x << endl;
+    cout << x << " Because link is always sad" << endl;
 
 }
